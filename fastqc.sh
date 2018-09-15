dd7ebff762166b769e899a1e13f33f9dd5f5bc74
NIU, SHENG-YONG <niu.shengyong@gmail.com>
2018-09-14 19:46:53 -0700

    Update fastqc.sh

--- fastqc.sh
+++ fastqc.sh
@@ -2,8 +2,8 @@
 module load fastqc
 
 
-for filename in  /restricted/projectnb/chengcancer/Simon/project/T2D/bulk_RNA-seq/merge_170415_NB501164_170506_NB501164/0raw/*.fastq.gz ;
+for filename in ./*.fastq.gz ;
 do
-fastqc -o /restricted/projectnb/chengcancer/Simon/project/T2D/bulk_RNA-seq/merge_170415_NB501164_170506_NB501164/1fastqc/  "$filename" ;
+fastqc -o ./1fastqc/  "$filename" ;
 
 done
